d85513215ca2396c8b8695063d990986c07b86d3
hschroedl <hansj.schroedl@gmail.com>
2016-02-16 13:54:36 +0100

    Write core logic

---
+++ main.js
@@ -0,0 +1,51 @@
+walk(document.body);
+
+function walk(node)
+{
+    // I stole this function from cloud-to-butt:
+    // https://github.com/panicsteve/cloud-to-butt
+
+    var child, next;
+
+    switch ( node.nodeType )
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child )
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}
+
+function handleText(textNode)
+{
+    var v = textNode.nodeValue;
+
+    v = v.replace(/\bBernie Sanders\b/g, getSynonym());
+    textNode.nodeValue = v;
+}
+
+function getSynonym(){
+    var possibleSynonyms = [
+        "Sernie Banders",
+        "Our Lord And Savior",
+        "That Old Guy",
+        "The Dude That Is Not Hillary",
+        "Grey Eel",
+        "The Only Consistent Politician",
+        "Super Bern",
+        "He-Who-Must-Not-Be-Shamed"
+    ];
+    var synonym = possibleSynonyms[Math.floor(Math.random()*possibleSynonyms.length)];
+    return synonym;
+}
